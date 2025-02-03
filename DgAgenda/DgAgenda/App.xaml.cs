b212a07c5beac22264d196dd6f4bd8963092da90
Douglas Ferreira <54293352+douglasfsouza@users.noreply.github.com>
2025-02-03 18:35:22 -0300

    Implementacao SQLite - DgAgenda

---
+++ DgAgenda/DgAgenda/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using DgAgenda.MVVM.Views;
+using DgAgenda.Services;
+
+namespace DgAgenda
+{
+    public partial class App : Application
+    {
+        public App(IAgendaService agendaService)
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new AgendaView(agendaService));
+        }
+    }
+}
---
+++ DgAgenda/DgAgenda/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DgAgenda
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
